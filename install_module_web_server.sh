bdcd57d5cdf5cfdf10d172854c5188267a7bbe72
TheSkyEye <mailforgithubtheskyeye@yopmail.com>
2016-10-01 02:49:31 +0200

    Add files via upload

---
+++ install_module_web_server.sh
@@ -0,0 +1,30 @@
+#MarionetteJS
+npm install backbone.marionette
+#ZeptoJS
+npm install zepto
+#MathJax
+npm install mathjax
+#Leaflet
+npm install leaflet
+#jQuery
+npm install jquery
+#Underscore
+npm install underscore
+#Handlebar
+npm install handlebars
+#Hammer
+npm install hammerjs
+#HeadJS
+npm install headjs
+#Vue.JS
+npm install vue
+#Require.JS
+npm install requirejs
+#Backbone.js
+npm install backbone
+#Video.js
+npm install video.js
+#SpinJS
+npm install spin
+#PDF.JS
+npm install pdfjs
